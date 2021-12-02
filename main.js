a58702d1c3f7d8fcb698d00cca123cc6d032fb83
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-12-02 22:37:49 +0100

    Now more primes are visible & changed style to dots.

--- main.js
+++ main.js
@@ -1,30 +1,69 @@
-function setup() {
-  createCanvas(800, 800);
-  background(0);
-}
-a=0
-b=0
+function setup() { 
 
+  createCanvas(800, 800); 
 
-function isPrime(num) {
-  for(var i = 2; i < num; i++)
-    if(num % i === 0) return 0;
-  return 255;
-}
+  background(0); 
 
+} 
 
+a=0 
 
-function draw() {
-  p = isPrime(a)
-  stroke(p,p,p)
-  if(a<800){
-    while(b<800){
-      line(a+1,b+1,a,b)
-      b+=1
-    }
-    b=0
-    a+=1
-  }else{
-    a=0
-  }
-}
+b=0 
+
+  
+
+  
+
+function isPrime(num) { 
+
+  for(var i = 2; i < num; i++) 
+
+    if(num % i === 0) return 0; 
+
+  return 255; 
+
+} 
+
+  
+
+  
+
+  
+
+function draw() { 
+
+   
+
+  if(a<800){ 
+
+    while(b<800){ 
+
+      z=(800*a)+b 
+
+      p = isPrime(z) 
+
+      stroke(p,p,p) 
+
+       
+
+      line(a+1,b+1,a,b) 
+
+       
+
+      b+=1 
+
+       
+
+    } 
+
+    b=0 
+
+    a+=1 
+
+  }else{ 
+
+    a=0 
+
+  } 
+
+} 
